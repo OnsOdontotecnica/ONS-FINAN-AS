6229d66475a663db8b64694c90fc72424e301809
OnsOdontotecnica <ons01odontotecnica@gmail.com>
2026-05-07 19:24:14 -0300

    Refactor service worker caching logic

--- sw.js
+++ sw.js
@@ -1,45 +1,19 @@
-const CACHE_NAME = 'ons-financeiro-v3';
+const CACHE_NAME = 'ons-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/launchericon-144x144.png',
-  '/launchericon-192x192.png',
-  '/launchericon-512x512.png'
+  './',
+  './index.html',
+  './manifest.json',
+  './launchericon-192x192.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Cache aberto');
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cache => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
